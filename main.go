a83979be99b6e1581e67a6dc2ade096d70ac24a0
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-01-08 03:53:46 +0330

    move handlers and renderTemplate functions

--- main.go
+++ main.go
@@ -2,34 +2,15 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about.page.tmpl")
-}
-
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
-}
 func main() {
 
 	http.HandleFunc("/home", Home)
 	http.HandleFunc("/about", About)
-	fmt.Printf("application is running on port%s", portNumber)
+	fmt.Printf("application is running on port%s  http://localhost:8080", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
