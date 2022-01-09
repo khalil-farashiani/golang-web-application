03f5efe800d6bbb5af9041a58c3faa1b5f99af08
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-01-10 01:36:57 +0330

    add use cache to the repo strtuct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,11 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+)
 
 //AppConfig holds the application config
 type AppConfig struct {
+	UseCache      bool
 	TemplateCache map[string]*template.Template
 }
