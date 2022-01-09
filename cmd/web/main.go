ed223451121d3bd86f05a50158f4479ee4c0365a
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-01-09 03:39:59 +0330

    get template cache data from configuration file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/khalil-farashiani/golang-web-application/pkg/config"
 	"github.com/khalil-farashiani/golang-web-application/pkg/handlers"
+	"github.com/khalil-farashiani/golang-web-application/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+	ts, err := render.CreateTemplateCache()
 
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = ts
 	http.HandleFunc("/home", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	fmt.Printf("application is running on port%s  http://localhost:8080", portNumber)
