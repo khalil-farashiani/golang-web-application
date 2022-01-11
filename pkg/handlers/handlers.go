d7e679b083e8f467286fd926adfb5748cc522a81
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-01-11 23:31:50 +0330

    work with session to get IP address

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,10 +29,18 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteAddress := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_addr", remoteAddress)
 	render.RenderTemplates(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplates(w, "about.page.tmpl", &models.TemplateData{})
+	stringMap := make(map[string]string)
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_addr")
+	stringMap["remote_IP"] = remoteIP
+
+	render.RenderTemplates(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
