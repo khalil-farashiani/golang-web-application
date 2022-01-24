07670c0f813d2c40b832790f9127d109c11c72ef
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-01-24 23:29:06 +0330

    start using ajax in project

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,11 +24,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
-	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
+	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/contact", handlers.Repo.Contact)
-	
+
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
