22abf9cd1287c7a5964a643731df7f61ede7d1ce
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-02 17:35:26 +0330

    add form structure to the project

---
+++ internal/forms/forms.go
@@ -0,0 +1,29 @@
+package forms
+
+import (
+	"net/http"
+	"net/url"
+)
+
+//Form creates a custom Form
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+//New initializes a Form struct
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+//Has checks if field is in form and not empty
+func (f *Form) Has(field string, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if x == "" {
+		return false
+	}
+	return true
+}
