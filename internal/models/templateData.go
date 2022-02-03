757232cb137a9334b66cf6a73f66008415b5474a
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-03 16:26:53 +0330

    add form attr to the form data structure

--- internal/models/templateData.go
+++ internal/models/templateData.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/khalil-farashiani/golang-web-application/internal/forms"
+
 //TemplateData send context to the template
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form      *forms.Form
 }
